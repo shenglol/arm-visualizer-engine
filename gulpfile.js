a1382fa108e28ed4f66ba7eb3b9e50fe9fb84039
Shenglong Li <shenglong.li@outlook.com>
2016-07-15 15:00:14 -0700

    Added version management gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -8,4 +8,7 @@ gulp.task('clean', gulp.parallel('clean:lib', 'clean:doc'));
 gulp.task('build', gulp.series('clean', gulp.parallel('tslint', 'jshint'), 'compile'));
 gulp.task('test', gulp.series('build', 'karma:single-run'));
 gulp.task('test:auto', gulp.series('build', 'karma:auto-run'));
+gulp.task('patch', gulp.series('version:patch'));
+gulp.task('feature', gulp.series('version:feature'));
+gulp.task('release', gulp.series('version:release'));
 gulp.task('default', gulp.series('build'));
