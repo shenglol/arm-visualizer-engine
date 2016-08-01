a33d25fd0b4d395b0ad29d67b223976b9580f6b1
Shenglong Li <shenglong.li@outlook.com>
2016-08-01 13:33:59 -0700

    Add commet for resourceId implementation

--- src/expressions/resource/resource-id-expression.ts
+++ src/expressions/resource/resource-id-expression.ts
@@ -13,6 +13,12 @@ export class ResourceIdExpression extends ExpressionBase {
 
         let resourceId = '';
 
+        // resourceId function should return an identifier in the following format:
+        //     /subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/{resourceProviderNamespace}/{resourceType}/{resourceName}
+        // Since we don't ask users for their subscriptionId and resourceGroupName,
+        // the identifier is returned in the following format instead:
+        //     /{subscriptionId}/{resourceGroupName}/{resourceType}/{resourceName}
+        // Please note that subscriptionId and resourceGroupName are optional operands
         for (let operand of this.operands) {
             resourceId += '/';
             resourceId += typeof operand === 'string'
