ec8087a73fe8237fecc4ad04a820256c171fe8c9
Shenglong Li <shenglong.li@outlook.com>
2016-08-09 12:32:51 -0700

    Fix error message not set bug in custom errors

--- src/shared/errors/expression-errors.ts
+++ src/shared/errors/expression-errors.ts
@@ -2,36 +2,42 @@ import * as ErrorMessages from './expression-error-messages';
 
 export class TooFewOperandsError extends Error {
     constructor() {
-        super(ErrorMessages.TOO_FEW_OPERANDS);
+        super();
+        this.message = ErrorMessages.TOO_FEW_OPERANDS;
     }
 }
 
 export class TooManyOperandsError extends Error {
     constructor() {
-        super(ErrorMessages.TOO_MANY_OPERANDS);
+        super();
+        this.message = ErrorMessages.TOO_MANY_OPERANDS;
     }
 }
 
 export class InvalidOperandTypeError extends Error {
     constructor(operand: string) {
-        super(ErrorMessages.INVALID_OPERAND_TYPE + ': ' + operand);
+        super();
+        this.message = ErrorMessages.INVALID_OPERAND_TYPE + ': ' + operand;
     }
 }
 
 export class InvalidExpSourceError extends Error {
     constructor() {
-        super(ErrorMessages.INVALID_EXP_SOURCE);
+        super();
+        this.message = ErrorMessages.INVALID_EXP_SOURCE;
     }
 }
 
 export class ExpContextNotFoundError extends Error {
     constructor(context: string) {
-        super(ErrorMessages.EXP_CONTEXT_NOT_FOUND + ': ' + context);
+        super();
+        this.message = ErrorMessages.EXP_CONTEXT_NOT_FOUND + ': ' + context;
     }
 }
 
 export class PropertyNotFoundError extends Error {
     constructor(property: string) {
-        super(ErrorMessages.PROPERTY_NOT_FOUND + ': ' + property);
+        super();
+        this.message = ErrorMessages.PROPERTY_NOT_FOUND + ': ' + property;
     }
 }
--- src/shared/errors/template-errors.ts
+++ src/shared/errors/template-errors.ts
@@ -2,18 +2,21 @@ import * as ErrorMessages from './template-error-messages';
 
 export class MissingTemplatePropertyError extends Error {
     constructor(property: string) {
-        super(ErrorMessages.MISSING_TEMPLATE_PROPERTY + ': ' + property);
+        super();
+        this.message = ErrorMessages.MISSING_TEMPLATE_PROPERTY + ': ' + property;
     }
 }
 
 export class DependencyNotFoundError extends Error {
     constructor(source: string) {
-        super(ErrorMessages.DEPENDENCY_NOT_FOUND + ' for expression: ' + source);
+        super();
+        this.message = ErrorMessages.DEPENDENCY_NOT_FOUND + ' for expression: ' + source;
     }
 }
 
 export class DuplicateDependenciesError extends Error {
     constructor(source: string) {
-        super(ErrorMessages.DUPLICATE_DEPENDENCIES + ' for expression: ' + source);
+        super();
+        this.message = ErrorMessages.DUPLICATE_DEPENDENCIES + ' for expression: ' + source;
     }
 }
